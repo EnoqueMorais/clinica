4e4c3bd322fd8223f08adf82fa1d542092fce1b3
Aluno <enoquemorais2010@gmail.com>
2018-11-24 02:06:26 -0300

    Funcionalidade listagem de cliente

--- atendimento/views.py
+++ atendimento/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from . models import Cliente
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html',{})
+
+def cliente_list(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'cliente/list.html',{'clientes':clientes})    
