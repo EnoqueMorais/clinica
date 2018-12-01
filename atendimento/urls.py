f54a026326e101aa444b75d1bbdf955ca4c81895
Aluno <enoquemorais2010@gmail.com>
2018-12-01 05:59:31 -0300

    Adicionando a funcionalidade mostra detalhe do  medico.

--- atendimento/urls.py
+++ atendimento/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home),
     path('cliente/', views.cliente_list),
     path('medico/', views.medico_list),
-    path('cliente/<int:cliente_id>',views.cliente_show)
+    path('cliente/<int:cliente_id>',views.cliente_show),
+    path('medico/<int:medico_id>',views.medico_show)
 ]
